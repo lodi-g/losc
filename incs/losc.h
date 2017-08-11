4d22388f4f7def69143dd3e41ad32ecae982abb6
Gregoire Lodi <gregoire.lodi@epitech.eu>
2017-08-11 11:43:29 +0200

    xfree

--- incs/losc.h
+++ incs/losc.h
@@ -3,6 +3,7 @@
 # define LOSC_H_
 
 # include <stdbool.h>
+# include "expand_macro.h"
 
 # define LOSC_NAME "losc"
 # define LOSC_DIRNAME LOSC_NAME
@@ -10,5 +11,8 @@
 # define UNUSED(x) (void)x
 # define LIKELY(x) __builtin_expect(x, 1)
 # define UNLIKELY(x) __builtin_expect(x, 0)
+
+# define xfree(...) \
+  do { COMMA_MACRO_EXPAND(free, __VA_ARGS__); } while (0)
 
 #endif
